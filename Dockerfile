7e2a6bbe75499b050c8f340783f66cea11ae37e3
Shreyasaraf01 <shreyasaraf765@gmail.com>
2025-07-26 16:00:08 +0530

    resolved

--- Dockerfile
+++ Dockerfile
@@ -21,6 +21,7 @@ RUN pip install --no-cache-dir -r requirements.txt
 # Copy project files
 COPY . .
 
-EXPOSE 10000
+EXPOSE 8000
 
-CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "10000"]
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
+
--- main.py
+++ main.py
@@ -148,6 +148,8 @@ async def compare_from_blob(req: CompareBlobRequest, user=Depends(get_current_us
         return {"error": str(e)}
 
 # --- Run App ---
+port = int(os.environ.get("PORT", 8000))
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
