96a88f0f130ba37db16fa439d54aecade620e432
Shreyasaraf01 <shreyasaraf765@gmail.com>
2025-07-27 15:23:25 +0530

    updated main.py

--- main.py
+++ main.py
@@ -160,5 +160,5 @@ def health_check():
 
 # --- Run App ---
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+   port = int(os.environ.get("PORT", 8000))
+   uvicorn.run("main:app", host="0.0.0.0", port=port)
