050db41f18990816e93d40d50bf212782562e390
Shreyasaraf01 <shreyasaraf765@gmail.com>
2025-07-27 18:14:20 +0530

    final

--- main.py
+++ main.py
@@ -150,5 +150,4 @@ def health_check():
 
 # --- Run App ---
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=7860)
